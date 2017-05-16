d6163aa4b36c270fa7e093aca235ec03c6ae6a01
lycoris0731 <aizu.s1230022@gmail.com>
2017-05-16 14:32:27 +0900

    Fix typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
-rm -fr app; goagen app -d github.com/soramitsu/doberman-server/design
-goagen controller --pkg='controller' --app-pkg='../app' -o controller -d github.com/soramitsu/doberman-server/design
-rm -fr swagger; goagen swagger -d github.com/soramitsu/doberman-server/design
-rm -fr client; goagen client -d github.com/soramitsu/doberman-server/design
+#!/bin/bash
+
+rm -fr app; goagen app -d github.com/soramitsu/iroha-gateway/design
+goagen controller --pkg='controller' --app-pkg='../app' -o controller -d github.com/soramitsu/iroha-gateway/design
+rm -fr swagger; goagen swagger -d github.com/soramitsu/iroha-gateway/design
+rm -fr client; goagen client -d github.com/soramitsu/iroha-gateway/design
